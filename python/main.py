4fa5abda92fbd2c38300f2d41f5c8173caf69a24
nmill <nmill@DESKTOP-E7RII8U>
2024-07-12 16:05:39 -0600

    Updated main.py to include some documentation

--- python/main.py
+++ python/main.py
@@ -1,30 +1,34 @@
-import board
 import curses
 import time
+import board
 
-
-def initScreen() -> curses.window:
+def init_screen() -> curses.window:
+    """
+    This function is called to initalize the curses screen.
+    This sets it up to be able to print colors and interact with the user.
+    @returns: curses.window : the current screen
+    """
     stdscr: curses.window = curses.initscr()
     curses.start_color()
     stdscr.keypad(True)
     curses.nocbreak()
     curses.noecho()
     curses.use_default_colors()
-    initColors()
+    init_colors()
     return stdscr
-def initColors():
+def init_colors():
     curses.init_pair(curses.COLOR_WHITE, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(curses.COLOR_BLUE, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(curses.COLOR_RED, curses.COLOR_RED, curses.COLOR_BLACK)
 
-def destroyScreen(stdscr: curses.window) -> None:
+def destroy_screen(stdscr: curses.window) -> None:
     stdscr.keypad(False)
     curses.echo()
     curses.cbreak()
     curses.endwin()
 
 def main():
-    stdscr: curses.window =  initScreen()
+    stdscr: curses.window =  init_screen()
     try:
         board1 = board.Board(stdscr)
         board1.printBoard()
@@ -38,7 +42,7 @@ def main():
     except:
         print('Error')
     finally:
-        destroyScreen(stdscr)
-    
+        destroy_screen(stdscr)
+
 if __name__ == '__main__':
     main()
