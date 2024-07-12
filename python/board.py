7530265df0ad5c05a8738b085bf0005190f3d1dd
nmill <nmill@DESKTOP-E7RII8U>
2024-07-12 17:09:39 -0600

    Updated documentation on all files, and mapped functions / variables to snake case

--- python/board.py
+++ python/board.py
@@ -1,9 +1,16 @@
+"""
+This module is used for the Board class.
+This is an object that is the basis for the Connect4 Game
+"""
+import curses
 import numpy as np
 from numpy._typing import NDArray
-import curses
 
-class Board(object):
-    
+class Board():
+    """
+    A class dedicated to the board object.
+    This object is where the pieces live and how moves are done in Connect4
+    """
     def __init__(self, screen):
         self._player1piece: int = 1
         self._player2piece: int = 2
@@ -12,13 +19,12 @@ class Board(object):
         self._board: NDArray = np.zeros((self._rows, self._cols))
         self._screen: curses.window = screen
 
-    def _getChar(self, val: int) -> str:
-        if(val == 0):
+    def _get_char(self, val: int) -> str:
+        if val == 0:
             return 'X'
-        else:
-            return 'O'
+        return 'O'
 
-    def _getColor(self, val: int) -> int:
+    def _get_color(self, val: int) -> int:
         if val == self._player1piece:
             return curses.color_pair(curses.COLOR_BLUE)
         if val == self._player2piece:
@@ -26,14 +32,18 @@ class Board(object):
         return curses.color_pair(curses.COLOR_WHITE)
 
 
-    def printBoard(self):
+    def print_board(self):
+        """
+        Prints the current board state to the terminal
+        """
         screen: curses.window = self._screen
         screen.clear()
         for y in range(self._cols):
             for x in range(self._rows):
                 xval = ((84 - self._rows * 2 + 1 ) // 2) + x*2 + 1
                 yval = ((21 - self._cols * 2 + 1 ) // 2) + y*2 + 1
-                screen.addch(yval, xval, self._getChar(int(self._board[y][x])), self._getColor(self._board[y][x]))
+                screen.addch(yval, xval, \
+                             self._get_char(int(self._board[y][x])), self._get_color(self._board[y][x]))
 
                 screen.addch(yval, xval-1, curses.ACS_VLINE)
                 screen.addch(yval, xval+1, curses.ACS_VLINE)
@@ -50,13 +60,18 @@ class Board(object):
             xval = ((84 - self._rows * 2 + 1 ) // 2) + x*2 + 1
             screen.addch(yval, xval, str(x + 1))
         screen.refresh()
-    def addMove(self, col: int, player1: bool) -> bool:
+    def add_move(self, col: int, player1: bool) -> bool:
+        """
+        Adds the move to the board.
+        @params:
+        col - the column is the labeled column to add the piece at (1 indexed).
+        player1 - true if it is the first player, false otherwise
+        """
         movecol = col - 1
-        moveTaken = False
+        move_taken = False
         for i in range(self._rows - 1, -1, -1):
-            if(self._board[i][movecol] == 0):
+            if self._board[i][movecol] == 0:
                 self._board[i][movecol] = self._player1piece if player1 else self._player2piece
-                moveTaken = True
+                move_taken = True
                 break
-        return moveTaken
-
+        return move_taken
--- python/main.py
+++ python/main.py
@@ -1,3 +1,6 @@
+"""
+The main function to operate the Connect4 Game
+"""
 import curses
 import time
 import board
@@ -17,27 +20,40 @@ def init_screen() -> curses.window:
     init_colors()
     return stdscr
 def init_colors():
+    """
+    This function creates the color pairs used in the board:
+    White - White on Black
+    Blue - Blue on Black
+    Red - Red on Black
+    """
     curses.init_pair(curses.COLOR_WHITE, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(curses.COLOR_BLUE, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(curses.COLOR_RED, curses.COLOR_RED, curses.COLOR_BLACK)
 
 def destroy_screen(stdscr: curses.window) -> None:
+    """
+    This function deinits the curses screen, allowing the terminal to resume operation as normal
+    @params: stdscr - The current curses.window
+    """
     stdscr.keypad(False)
     curses.echo()
     curses.cbreak()
     curses.endwin()
 
 def main():
+    """
+    A main function to operate the game loop
+    """
     stdscr: curses.window =  init_screen()
     try:
         board1 = board.Board(stdscr)
-        board1.printBoard()
+        board1.print_board()
         time.sleep(5)
-        board1.addMove(1, True)
-        board1.printBoard()
+        board1.add_move(1, True)
+        board1.print_board()
         time.sleep(5)
-        board1.addMove(1, False)
-        board1.printBoard()
+        board1.add_move(1, False)
+        board1.print_board()
         time.sleep(5)
     except:
         print('Error')
