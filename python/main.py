c3d99702283ed8a64c2586236f6a688e2d441780
nmill <nmill@DESKTOP-E7RII8U>
2024-07-16 19:35:40 -0600

    Added inital screen and logic to game loop.

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 """
 The main function to operate the Connect4 Game
 """
+import sys
 import curses
 import time
 import board
@@ -14,7 +15,7 @@ def init_screen() -> curses.window:
     stdscr: curses.window = curses.initscr()
     curses.start_color()
     stdscr.keypad(True)
-    curses.nocbreak()
+    curses.cbreak()
     curses.noecho()
     curses.use_default_colors()
     init_colors()
@@ -38,13 +39,36 @@ def destroy_screen(stdscr: curses.window) -> None:
     """
     stdscr.keypad(False)
     curses.echo()
-    curses.cbreak()
+    curses.nocbreak()
     curses.endwin()
 
 def init_game(stdscr: curses.window):
+    """
+    The function to start the game
+    """
     stdscr.addstr(0, 10, "Welcome to Connect4")
+    stdscr.addstr(1, 10, "Will you have (1) or (2) Players? (q)uit")
     stdscr.refresh()
-    pass
+    c = stdscr.getch()
+    while c != ord('1') and c != ord('2') and c != ord('q'):
+        stdscr.addstr(2, 10, "Please enter a valid key")
+        stdscr.addstr(3, 10, f"{c}")
+        stdscr.addstr(4, 10, f"{ord('1')}")
+        stdscr.addstr(5, 10, f"{ord('2')}")
+        stdscr.addstr(5, 10, f"{ord('q')}")
+        stdscr.refresh()
+        c = stdscr.getch()
+    stdscr.clear()
+    stdscr.addstr(2, 10, "You have entered a valid key")
+    stdscr.refresh()
+    if c == ord('1'):
+        return 1
+    if c == ord('2'):
+        return 2
+    if c == ord('q'):
+        return 0
+    stdscr.refresh()
+    return -1
 
 def main():
     """
@@ -53,6 +77,14 @@ def main():
     stdscr: curses.window =  init_screen()
     try:
         board1 = board.Board(stdscr)
+        count = init_game(stdscr)
+        if count == 1:
+            pass
+        if count == 2:
+            pass
+        if count == 0:
+            destroy_screen(stdscr)
+            sys.exit()
         board1.print_board()
         time.sleep(5)
         board1.add_move(1, True)
@@ -68,4 +100,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-     
