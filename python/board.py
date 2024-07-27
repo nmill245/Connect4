e7e52064eb213a21881e8ae89d8b0210c4caa56f
nmill <nmill@DESKTOP-E7RII8U>
2024-07-27 10:01:04 -0600

    Updated game loop to allow for computer to play moves.

--- python/board.py
+++ python/board.py
@@ -26,6 +26,10 @@ class Board():
             names.append('Computer')
         self._names = names
 
+    def copy(self):
+        board_copy = Board(self._screen, self._names.copy())
+        board_copy._board = self._board.copy()
+        return board_copy
     def _get_char(self, val: int) -> str:
         if val == 0:
             return ' '
@@ -40,23 +44,17 @@ class Board():
             return curses.color_pair(curses.COLOR_YELLOW)
         return curses.color_pair(curses.COLOR_WHITE)
 
-    def _scoresection(self, startcol: int, startrow: int, player1: bool) -> int:
-        return startcol - startrow * (-1 * player1)
-
-    def score_board(self, player1_turn) -> int:
+    def score_board(self, player1_turn: bool) -> int:
         """
         A function to score the board at a given state
         """
         if self._haswon:
-            return -100_000 * (-1 * (player1_turn == self._player_1_win))
+            if (self._player_1_win and player1_turn) or (not self._player_1_win and not player1_turn):
+                return 100_000
+            return -100_000
         player1score: int = 0
         player2score: int = 0
-        for col in range(3, self._cols):
-            for row in range(3, self._rows):
-                player1score = self._scoresection(col - 3, row - 3, True)
-                player2score = self._scoresection(col - 3, row - 3, False)
-
-        return player1score - player2score * (-1 * player1_turn)
+        return player1score - player2score
     def print_board(self, player1_turn):
         """
         Prints the current board state to the terminal
@@ -110,6 +108,21 @@ class Board():
                 break
         return move_taken
 
+    def remove_move(self, col: int, player1: bool) -> bool:
+        """
+        Adds the move to the board.
+        @params:
+        col - the column is the labeled column to add the piece at (1 indexed).
+        player1 - true if it is the first player, false otherwise
+        """
+        movecol = col - 1
+        move_removed = False
+        for i in range(self._rows - 1, -1, -1):
+            if (self._board[i][movecol] == self._player1piece and player1) or (self._board[i][movecol] == self._player2piece and not player1):
+                self._board[i][movecol] = 0
+                move_removed = True
+                break
+        return move_removed
     def check_win(self) -> bool:
         """
         Returns true if the board has a winning position, false otherwise
--- python/main.py
+++ python/main.py
@@ -14,27 +14,30 @@ def min_max(maximizing_player: bool, alpha: float, beta: float, max_depth: int, 
     """
     An algorithm to return the best move to make at a given board position, looking 5 moves in advance
     """
-    if max_depth == 0 or gameboard.check_win():
-        return (-1, gameboard.score_board(maximizing_player))
+    board_copy = gameboard.copy()
+    if max_depth == 0 or board_copy.check_win():
+        return (-1, board_copy.score_board(maximizing_player))
     if maximizing_player:
         score:float = -math.inf
-        move_list: list[int] = gameboard.get_moves()
+        move_list: list[int] = board_copy.get_moves()
         commited_move:int = -1
         for move in move_list:
             commited_move = move
-            gameboard.add_move(move + 1, maximizing_player)
-            move, score = min_max(not maximizing_player, alpha, beta, max_depth - 1, gameboard)
+            board_copy.add_move(move + 1, maximizing_player)
+            move, score = min_max(not maximizing_player, alpha, beta, max_depth - 1, board_copy)
+            board_copy.remove_move(move + 1, maximizing_player)
             if score > beta:
                 break
             alpha = max(alpha, score)
         return (commited_move, score)
     score:float = math.inf
-    move_list: list[int] = gameboard.get_moves()
+    move_list: list[int] = board_copy.get_moves()
     commited_move: int = -1
     for move in move_list:
         commited_move = move
-        gameboard.add_move(move + 1, maximizing_player)
-        move, score = min_max(not maximizing_player, alpha, beta, max_depth - 1, gameboard)
+        board_copy.add_move(move + 1, maximizing_player)
+        move, score = min_max(not maximizing_player, alpha, beta, max_depth - 1, board_copy)
+        board_copy.remove_move(move + 1, maximizing_player)
         if score < alpha:
             break
         beta = score
@@ -49,11 +52,12 @@ def main():
     try:
         names = screen.init_game(stdscr)
         while len(names) != 0:
-            game_board = board.Board(stdscr, names)
+            game_board = board.Board(stdscr, names.copy())
             player1_turn : bool = rand.randint(0, 100) < 50
             while len(game_board.get_moves()) != 0 and not game_board.check_win():
                 if player1_turn or len(names) == 2:
                     game_board.print_board(player1_turn)
+                    stdscr.addstr(0, 0, f"{len(names)}: {names}")
                     move_col = screen.get_user_move(stdscr, game_board)
                     if move_col == 'q':
                         screen.destroy_screen(stdscr)
@@ -61,7 +65,10 @@ def main():
                     game_board.add_move(int(move_col), player1_turn)
                     player1_turn = not player1_turn
                 else:
+                    game_board.print_board(player1_turn)
                     move_col, _ = min_max(player1_turn, -math.inf, math.inf, 5, game_board)
+                    game_board.add_move(move_col + 1, player1_turn)
+                    player1_turn = not player1_turn
             game_board.print_board(not player1_turn)
             time.sleep(1)
             names = screen.init_game(stdscr)
